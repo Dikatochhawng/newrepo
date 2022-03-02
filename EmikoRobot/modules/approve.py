8dd812722552c904680ab973565c2205e9917db6
Dikatochhawng <93668779+Dikatochhawng@users.noreply.github.com>
2022-03-02 14:16:39 +0530

    Update approve.py

--- EmikoRobot/modules/approve.py
+++ EmikoRobot/modules/approve.py
@@ -22,7 +22,7 @@ def approve(update, context):
     user_id = extract_user(message, args)
     if not user_id:
         message.reply_text(
-            "I don't know who you're talking about, you're going to need to specify a user!",
+            "I sawi awmzia ka hrethiam lo, chiang takin sawi rawh!",
         )
         return ""
     try:
@@ -31,18 +31,18 @@ def approve(update, context):
         return ""
     if member.status in ("administrator", "creator"):
         message.reply_text(
-            "User is already admin - locks, blocklists, and antiflood already don't apply to them.",
+            "User hi admin ani sa tawh a - locks, blocklists, leh antiflood te hi a chung ah apply ani lo.",
         )
         return ""
     if sql.is_approved(message.chat_id, user_id):
         message.reply_text(
-            f"[{member.user['first_name']}](tg://user?id={member.user['id']}) is already approved in {chat_title}",
+            f"[{member.user['first_name']}](tg://user?id={member.user['id']}) hi {chat_title} ah approve ani e.",
             parse_mode=ParseMode.MARKDOWN,
         )
         return ""
     sql.approve(message.chat_id, user_id)
     message.reply_text(
-        f"[{member.user['first_name']}](tg://user?id={member.user['id']}) has been approved in {chat_title}! They will now be ignored by automated admin actions like locks, blocklists, and antiflood.",
+        f"[{member.user['first_name']}](tg://user?id={member.user['id']}) hi {chat_title} ah approve ani e! Heng locks, blocklists, and antiflood te hian a chung ah awmzia a neilo ang.",
         parse_mode=ParseMode.MARKDOWN,
     )
     log_message = (
@@ -66,7 +66,7 @@ def disapprove(update, context):
     user_id = extract_user(message, args)
     if not user_id:
         message.reply_text(
-            "I don't know who you're talking about, you're going to need to specify a user!",
+            "I sawi awmzia ka hrethiam lo, chiang takin sawi rawh!",
         )
         return ""
     try:
@@ -74,14 +74,14 @@ def disapprove(update, context):
     except BadRequest:
         return ""
     if member.status in ("administrator", "creator"):
-        message.reply_text("This user is an admin, they can't be unapproved.")
+        message.reply_text("He user hi admin ani a, unapprove theih a nilo.")
         return ""
     if not sql.is_approved(message.chat_id, user_id):
-        message.reply_text(f"{member.user['first_name']} isn't approved yet!")
+        message.reply_text(f"{member.user['first_name']} hi approve ala nilo!")
         return ""
     sql.disapprove(message.chat_id, user_id)
     message.reply_text(
-        f"{member.user['first_name']} is no longer approved in {chat_title}.",
+        f"{member.user['first_name']} hi {chat_title} ah hian approve ani tawhlo.",
     )
     log_message = (
         f"<b>{html.escape(chat.title)}:</b>\n"
@@ -98,13 +98,13 @@ def approved(update, context):
     message = update.effective_message
     chat_title = message.chat.title
     chat = update.effective_chat
-    msg = "The following users are approved.\n"
+    msg = "Heng users te hi approve te chu anni.\n"
     approved_users = sql.list_approved(message.chat_id)
     for i in approved_users:
         member = chat.get_member(int(i.user_id))
         msg += f"- `{i.user_id}`: {member.user['first_name']}\n"
     if msg.endswith("approved.\n"):
-        message.reply_text(f"No users are approved in {chat_title}.")
+        message.reply_text(f"members tumah {chat_title} ah hian approve an nilo.")
         return ""
     message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
 
@@ -118,16 +118,16 @@ def approval(update, context):
     member = chat.get_member(int(user_id))
     if not user_id:
         message.reply_text(
-            "I don't know who you're talking about, you're going to need to specify a user!",
+            "I sawi awmzia ka hrethiam lo, chiang takin sawi rawh!",
         )
         return ""
     if sql.is_approved(message.chat_id, user_id):
         message.reply_text(
-            f"{member.user['first_name']} is an approved user. Locks, antiflood, and blocklists won't apply to them.",
+            f"{member.user['first_name']} hi approve ani a. Locks, antiflood, and blocklists te hian awmzia a neilo.",
         )
     else:
         message.reply_text(
-            f"{member.user['first_name']} is not an approved user. They are affected by normal commands.",
+            f"{member.user['first_name']} hi approve ani lo. command hmang in an chung ah action lak theih ani.",
         )
 
 
@@ -137,7 +137,7 @@ def unapproveall(update: Update, context: CallbackContext):
     member = chat.get_member(user.id)
     if member.status != "creator" and user.id not in DRAGONS:
         update.effective_message.reply_text(
-            "Only the chat owner can unapprove all users at once.",
+            "Group siamtu chiah hian members te a rualin vawikhat ah a unapprove thei.",
         )
     else:
         buttons = InlineKeyboardMarkup(
@@ -157,7 +157,7 @@ def unapproveall(update: Update, context: CallbackContext):
             ],
         )
         update.effective_message.reply_text(
-            f"Are you sure you would like to unapprove ALL users in {chat.title}? This action cannot be undone.",
+            f"I chiang maw he Group {chat.title} a members te hi unapprove vek i duh maw? A sut theih tawhloh ania.",
             reply_markup=buttons,
             parse_mode=ParseMode.MARKDOWN,
         )
@@ -181,29 +181,28 @@ def unapproveall_btn(update: Update, context: CallbackContext):
             query.answer("Only owner of the chat can do this.")
 
         if member.status == "member":
-            query.answer("You need to be admin to do this.")
+            query.answer("He thil ti tur hi chuan admin i nih angai.")
     elif query.data == "unapproveall_cancel":
         if member.status == "creator" or query.from_user.id in DRAGONS:
-            message.edit_text("Removing of all approved users has been cancelled.")
+            message.edit_text("approved user te remove na chu cancel ani.")
             return ""
         if member.status == "administrator":
-            query.answer("Only owner of the chat can do this.")
+            query.answer("He Group siamtu chauh hian he thil hi chu a ti thei.")
         if member.status == "member":
-            query.answer("You need to be admin to do this.")
+            query.answer("He thil ti tur hi chuan admin i nih angai.")
 
 
 __help__ = """
-Sometimes, you might trust a user not to send unwanted content.
-Maybe not enough to make them admin, but you might be ok with locks, blacklists, and antiflood not applying to them.
+Member nazawng ten Group an tihbuai loh nan he thil hi chu a tangkai.
 
-That's what approvals are for - approve of trustworthy users to allow them to send
+Chuvang a approval hi siam ani.
 
-*Admin commands:*
-❂ /approval*:* Check a user's approval status in this chat.
-❂ /approve*:* Approve of a user. Locks, blacklists, and antiflood won't apply to them anymore.
-❂ /unapprove*:* Unapprove of a user. They will now be subject to locks, blacklists, and antiflood again.
-❂ /approved*:* List all approved users.
-❂ /unapproveall*:* Unapprove *ALL* users in a chat. This cannot be undone.
+*Admin Tan Chiah:*
+❂ /approval*:* Approve leh approve loh en na (user message reply emaw command zawh ah username emaw ID dah tur).
+❂ /approve*:* Member approve na (Approve hian Admin ang in hrem na tamtak an pumpelh thei).
+❂ /unapprove*:* Approve tawh sa sut leh na.
+❂ /approved*:* Approve tawh ho en na.
+❂ /unapproveall*:* Approve tawh sa a rual a sut leh na..
 """
 
 APPROVE = DisableAbleCommandHandler("approve", approve, run_async=True)
