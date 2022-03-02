2c8ac803e18c7a1b5ad3eb091097789eba336729
Dikatochhawng <93668779+Dikatochhawng@users.noreply.github.com>
2022-03-02 22:30:35 +0530

    Update paste.py

--- EmikoRobot/modules/paste.py
+++ EmikoRobot/modules/paste.py
@@ -39,7 +39,7 @@ async def isPreviewUp(preview: str) -> bool:
 async def paste_func(_, message):
     if not message.reply_to_message:
         return await message.reply_text(
-            "Reply To A Message With /paste"
+            "Message hi /paste ti in reply rawh"
         )
     m = await message.reply_text("Pasting...")
     if message.reply_to_message.text:
@@ -48,10 +48,10 @@ async def paste_func(_, message):
         document = message.reply_to_message.document
         if document.file_size > 1048576:
             return await m.edit(
-                "You can only paste files smaller than 1MB."
+                "1MB aia te chiah a paste theih."
             )
         if not pattern.search(document.mime_type):
-            return await m.edit("Only text files can be pasted.")
+            return await m.edit("Text files chiah a paste theih.")
         doc = await message.reply_to_message.download()
         async with aiofiles.open(doc, mode="r") as f:
             content = await f.read()
