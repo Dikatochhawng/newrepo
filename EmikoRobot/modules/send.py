728c81d53eab06f13a462078b4effc4cc79eb7bf
Dikatochhawng <93668779+Dikatochhawng@users.noreply.github.com>
2022-03-04 22:29:41 +0530

    Update send.py

--- EmikoRobot/modules/send.py
+++ EmikoRobot/modules/send.py
@@ -1,32 +1,9 @@
+from pyrogram import filters
+from wbb import app
 
 
-from telegram.ext import run_async
-
-
-from EmikoRobot import dispatcher
-
-from EmikoRobot.modules.disable import DisableAbleCommandHandler
-from EmikoRobot.modules.helper_funcs.alternate import send_message
-from EmikoRobot.modules.helper_funcs.chat_status import user_admin
-
-
-@run_async
-@user_admin
-def send(update, context):
-    args = update.effective_message.text.split(None, 1)
-    creply = args[1]
-    send_message(update.effective_message, creply)
-
-
-
-
-# Stats Module
-
-
-
-
-
-ADD_CCHAT_HANDLER = DisableAbleCommandHandler("snd", send)
-dispatcher.add_handler(ADD_CCHAT_HANDLER)
-__command_list__ = ["snd"]
-__handlers__ = [ADD_CCHAT_HANDLER]
+@app.on_message(filters.command("snd"))
+async def send(_, message):
+  rsr = message.text.split(None, 1)[1]
+  await app.send_message(message.chat.id, text=rsr, disable_web_page_preview=True)
+  await message.delete()
