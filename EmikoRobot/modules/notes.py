5b69ae01a0b7884dadf807e68071df5eb73bbf57
Dikatochhawng <93668779+Dikatochhawng@users.noreply.github.com>
2022-03-04 12:57:03 +0530

    Update notes.py

--- EmikoRobot/modules/notes.py
+++ EmikoRobot/modules/notes.py
@@ -377,7 +377,7 @@ def list_notes(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     note_list = sql.get_all_chat_notes(chat_id)
     notes = len(note_list) + 1
-    msg = "Note kohchhuah dan `/notenumber` emaw `#notename` \n\n  *ID*    *Note* \n"
+    msg = "Note i kohchhuah duh chuan `/notenumber` emaw `#note hming` emaw khitiang khian i kochhuak thei ang./ leh # khi a command na aw.\n\n  *ID*    *Note* \n"
     for note_id, note in zip(range(1, notes), note_list):
         if note_id < 10:
             note_name = f"`{note_id:2}.`  `#{(note.name.lower())}`\n"
